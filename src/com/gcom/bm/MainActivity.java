f7436a3bbaa3dc6600aaab8703a5fe0955644868
mmcomp <mad_moon_lover@yahoo.com>
2013-09-12 13:14:16 +0430

    Add click to first button

--- src/com/gcom/bm/MainActivity.java
+++ src/com/gcom/bm/MainActivity.java
@@ -3,13 +3,20 @@ package com.gcom.bm;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	ImageButton imageButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		addListenerOnButton();
 	}
 
 	@Override
@@ -18,5 +25,21 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	public void addListenerOnButton() {
+		 
+		imageButton = (ImageButton) findViewById(R.id.lightButton);
+		//imageButton.setOnClickListener( );
+		imageButton.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+			   Toast.makeText(MainActivity.this,
+				"ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+ 
+			}
+ 
+		});
+ 
+	}
 }
